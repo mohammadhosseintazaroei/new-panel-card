7a3f2af86faf2f21d071a2541a6fe3320bf150b5
mohammad753 <mh@local.net>
2023-05-24 13:35:05 -0700

    some change

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,13 +1,3 @@
 import InfoCard from "./info-card";
 
 export {InfoCard };
-// import { Theme } from "@mui/material/styles";
-// export const titleStyle = (theme: Theme) => ({
-//   fontSize: "14px",
-//   color:theme.customPrimary.main
-// });
-// // export const myElementStyle = (theme: Theme) => ({
-// //   backgroundColor: theme.palette.primary.main,
-// //   fontSize: theme.typography.fontSize,
-// //   // Add more custom styles as needed
-// // });
